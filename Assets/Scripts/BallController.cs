d2a84a50b5577dffffa3e1ba7a6042079176a897
Antonio Vazquez-Mackay <73569624+Tonydvm@users.noreply.github.com>
2025-02-07 12:36:05 -0800

    basic bolwing and pins

---
+++ Assets/Scripts/BallController.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class BallController : MonoBehaviour
+{
+    [SerializeField] private float force = 1f;
+    [SerializeField] private InputManager inputManager;
+
+    private Rigidbody ballRB;
+    private bool isBallLaunched;
+
+    void Start()
+    {
+        //Grabbing a reference to RigidBody
+        ballRB = GetComponent<Rigidbody>();
+
+        // Add a listener to the OnSpacePressed event.
+        // When the space key is pressed the
+        // LaunchBall method will be called.
+        inputManager.OnSpacePressed.AddListener(LaunchBall);
+    }
+
+
+    private void LaunchBall()
+    {
+        if (isBallLaunched) return;
+        isBallLaunched = true;
+        ballRB.AddForce(transform.forward * force, ForceMode.Impulse);
+    }
+
+}
